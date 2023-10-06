f0eaa9c9f6b361f8e98b3553aeb2961aafe7a388
KatyaSabanci <kateryna.kosyachenko@gmail.com>
2023-10-06 22:22:09 +0300

    1task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+let timerId;
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+});
---
+++ src/js/02-timer.js
@@ -0,0 +1,5 @@
+const startBtn = document.querySelector(['.data-start']);
+const day = document.querySelector(['.data-days']);
+const hour = document.querySelector(['.data-hours']);
+const minute = document.querySelector(['.data-minutes']);
+const second = document.querySelector(['.data-seconds']);
