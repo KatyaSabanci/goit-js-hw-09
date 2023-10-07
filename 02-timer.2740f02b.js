9d2b46ca504bb6b368e1d2f12d0b0d2e0035ac64
KatyaSabanci <132681078+KatyaSabanci@users.noreply.github.com>
2023-10-07 20:55:55 +0000

    Deploying to gh-pages from @ KatyaSabanci/goit-js-hw-09@6ba6575d4439d321b28d3b9b368d89d4e3a9fa5b 🚀

--- 02-timer.2740f02b.js
+++
@@ -1,2 +0,0 @@
-document.querySelector([".data-start"]),document.querySelector([".data-days"]),document.querySelector([".data-hours"]),document.querySelector([".data-minutes"]),document.querySelector([".data-seconds"]);
-//# sourceMappingURL=02-timer.2740f02b.js.map
--- 02-timer.942e5269.js
+++
@@ -1,2 +0,0 @@
-document.querySelector([".data-start"]),document.querySelector([".data-days"]),document.querySelector([".data-hours"]),document.querySelector([".data-minutes"]),document.querySelector([".data-seconds"]);
-//# sourceMappingURL=02-timer.942e5269.js.map
